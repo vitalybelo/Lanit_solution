88011d13de0c8026565a731279671be0d73b164c
Vitaly <vitaly.belo@gmail.com>
2023-01-24 12:18:35 +0300

    check_out

--- src/main/java/employee/controllers/EmployeeController.java
+++ src/main/java/employee/controllers/EmployeeController.java
@@ -7,11 +7,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/employee")
 public class EmployeeController {
 
     private final EmployeeStorage employees = new EmployeeStorage();
 
-    @PostMapping("/employee/")
+    @PostMapping("/")
     public int add(Employee employee) {
         if (!employee.getName().isEmpty()) {
             // возвращаем id добавленного сотрудника
@@ -20,22 +21,22 @@ public class EmployeeController {
         return -1; // данных в запросе нет, добавлять не будем
     }
 
-    @GetMapping("/employee/")
+    @GetMapping("/")
     public List<Employee> list() {
         return employees.findAll();
     }
 
-    @GetMapping("/employee/{id}")
+    @GetMapping("/{id}")
     public Employee get(@PathVariable int id) {
         return employees.find(id);
     }
 
-    @DeleteMapping("/employee/{id}")
+    @DeleteMapping("/{id}")
     public boolean delete(@PathVariable int id) {
         return employees.delete(id); // возвращает true в случае успеха
     }
 
-    @PutMapping("/employee/")
+    @PutMapping("/")
     public int edit(Employee employee) {
         return employees.edit(employee);
     }
