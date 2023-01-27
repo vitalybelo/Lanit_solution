8dea9985989fd9df729e1c69cba355e3a51dded0
Vitaly <vitaly.belo@gmail.com>
2023-01-27 15:24:04 +0300

    check_out

--- src/main/java/employee/controllers/EmployeeController.java
+++ src/main/java/employee/controllers/EmployeeController.java
@@ -2,17 +2,18 @@ package employee.controllers;
 
 import employee.model.Employee;
 import employee.model.EmployeeStorage;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/employee/")
 public class EmployeeController {
 
     private final EmployeeStorage employees = new EmployeeStorage();
 
-    @PostMapping("/")
+    @PostMapping
     public int add(Employee employee) {
         if (!employee.getName().isEmpty()) {
             // возвращаем id добавленного сотрудника
@@ -21,22 +22,26 @@ public class EmployeeController {
         return -1; // данных в запросе нет, добавлять не будем
     }
 
-    @GetMapping("/")
-    public List<Employee> list() {
-        return employees.findAll();
+    @GetMapping
+    public ResponseEntity<List<Employee>> list() {
+        return ResponseEntity.ok(employees.findAll());
     }
 
-    @GetMapping("/{id}")
-    public Employee get(@PathVariable int id) {
-        return employees.find(id);
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> get(@PathVariable int id) {
+        Employee employee = employees.find(id);
+        if (employee == null) {
+            return ResponseEntity.ok(null);
+        }
+        return ResponseEntity.ok(employees.find(id));
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("{id}")
     public boolean delete(@PathVariable int id) {
         return employees.delete(id); // возвращает true в случае успеха
     }
 
-    @PutMapping("/")
+    @PutMapping
     public int edit(Employee employee) {
         return employees.edit(employee);
     }
